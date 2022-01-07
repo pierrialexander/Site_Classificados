f7caf56f8a065852e831558fab205648af364d99
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2022-01-07 13:32:44 -0300

    Configuração inicial e particionamento de código
    Configuração inicial e particionamento de código

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php require_once 'pages/header.php'; ?>
+ 
+
+<!--INICIO DO CONTAINER-->
+  <div class="container-fluid">
+    <br>
+    <!--INICIO DO JUMBOTRON-->
+    <div class="jumbotron">,
+      <h5>Seja bem vindo!</h5>
+      <h2>Nós temos hoje 999 anúncios</h2>
+      <p>E mais de 222 usuários cadastrados</p>
+    </div>
+    <!--FINAL DO JUMBOTRON-->
+
+    <!--INICIO DO GRID-->
+    <div class="row">
+      <div class="col-sm-3">
+        <h4>Pesquisa Avançada</h4>
+      </div>
+      <div class="col-sm-9">
+        <h4>Últimos Anúncios</h4>
+      </div>
+    </div>
+  </div>
+  <!--FINAL DO CONTAINER-->
+  
+  
+    
+  <?php require_once 'pages/footer.php'; ?>
---
+++ pages/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Sistema de Classificados</title>
+</head>
+<body>
+
+  <!--INICIO DA NAVBAR-->
+  <nav class="navbar bg-success">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a href="./" class="nav-link text-light">Classificados Lucav Publicidades</a>
+      </div>
+      <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a href="#" class="nav-link active text-light font-weight-bold">Cadastre-se</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active text-light font-weight-bold">Login</a>
+          </li>
+      </ul>
+    </div>  
+  </nav>
+  <!--FINAL DA NAVBAR-->
